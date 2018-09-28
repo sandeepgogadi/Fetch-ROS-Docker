e9c2cee4bc34867ea2aaac0479775e5d8251cb76
sandeepgogadi <sandeepgogadi@yahoo.com>
2018-09-27 22:07:47 -0700

    pick_place-5

--- applications/scripts/pick_place.py
+++ applications/scripts/pick_place.py
@@ -234,60 +234,61 @@ if __name__ == "__main__":
     head_action = PointHeadClient()
     grasping_client = GraspingClient()
 
-    # Move the base to be in front of the table
-    # Demonstrates the use of the navigation stack
-    rospy.loginfo("Moving to table...")
-    move_base.goto(2.250, 3.118, 0.0)
-    move_base.goto(2.750, 3.118, 0.0)
+    for i in range(4):
+        # Move the base to be in front of the table
+        # Demonstrates the use of the navigation stack
+        rospy.loginfo("Moving to table...")
+        move_base.goto(2.250, 3.118, 0.0)
+        move_base.goto(2.750, 3.118, 0.0)
 
-    # Raise the torso using just a controller
-    rospy.loginfo("Raising torso...")
-    torso_action.move_to([0.4, ])
+        # Raise the torso using just a controller
+        rospy.loginfo("Raising torso...")
+        torso_action.move_to([0.4, ])
 
-    # Point the head at the cube we want to pick
-    head_action.look_at(3.7, 3.18, 0.0, "map")
+        # Point the head at the cube we want to pick
+        head_action.look_at(3.7, 3.18, 0.0, "map")
 
-    # Get block to pick
-    while not rospy.is_shutdown():
-        rospy.loginfo("Picking object...")
-        grasping_client.updateScene()
-        cube, grasps = grasping_client.getGraspableCube()
-        if cube == None:
-            rospy.logwarn("Perception failed.")
-            continue
+        # Get block to pick
+        while not rospy.is_shutdown():
+            rospy.loginfo("Picking object...")
+            grasping_client.updateScene()
+            cube, grasps = grasping_client.getGraspableCube()
+            if cube == None:
+                rospy.logwarn("Perception failed.")
+                continue
 
-        # Pick the block
-        if grasping_client.pick(cube, grasps):
-            break
-        rospy.logwarn("Grasping failed.")
+            # Pick the block
+            if grasping_client.pick(cube, grasps):
+                break
+            rospy.logwarn("Grasping failed.")
 
-    # Tuck the arm
-    grasping_client.tuck()
+        # Tuck the arm
+        grasping_client.tuck()
 
-    # Lower torso
-    rospy.loginfo("Lowering torso...")
-    torso_action.move_to([0.0, ])
+        # Lower torso
+        rospy.loginfo("Lowering torso...")
+        torso_action.move_to([0.0, ])
 
-    # Move to second table
-    rospy.loginfo("Moving to second table...")
-    move_base.goto(-3.53, 3.75, 1.57)
-    move_base.goto(-3.53, 4.15, 1.57)
+        # Move to second table
+        rospy.loginfo("Moving to second table...")
+        move_base.goto(-3.53, 3.75, 1.57)
+        move_base.goto(-3.53, 4.15, 1.57)
 
-    # Raise the torso using just a controller
-    rospy.loginfo("Raising torso...")
-    torso_action.move_to([0.4, ])
+        # Raise the torso using just a controller
+        rospy.loginfo("Raising torso...")
+        torso_action.move_to([0.4, ])
 
-    # Place the block
-    while not rospy.is_shutdown():
-        rospy.loginfo("Placing object...")
-        pose = PoseStamped()
-        pose.pose = cube.primitive_poses[0]
-        pose.pose.position.z += 0.05
-        pose.header.frame_id = cube.header.frame_id
-        if grasping_client.place(cube, pose):
-            break
-        rospy.logwarn("Placing failed.")
+        # Place the block
+        while not rospy.is_shutdown():
+            rospy.loginfo("Placing object...")
+            pose = PoseStamped()
+            pose.pose = cube.primitive_poses[0]
+            pose.pose.position.z += 0.05
+            pose.header.frame_id = cube.header.frame_id
+            if grasping_client.place(cube, pose):
+                break
+            rospy.logwarn("Placing failed.")
 
-    # Tuck the arm, lower the torso
-    grasping_client.tuck()
-    torso_action.move_to([0.0, ])
+        # Tuck the arm, lower the torso
+        grasping_client.tuck()
+        torso_action.move_to([0.0, ])
